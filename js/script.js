b392b47374858e408374b781ebd11afcab30559f
swifferwarrior <kdhailey21@gmail.com>
2020-06-06 02:29:45 -0500

    Stylize assignment as a Game Boy handheld.
    Added a reset button.
    Added REady Player 2 font.
    Centered the pixels as percentages.
    Added Game Boy overlay and startup sound.

--- js/script.js
+++ js/script.js
@@ -3,28 +3,32 @@ const main = document.getElementById('main');
 const div = document.createElement('div');
 const square = document.createElement('div');
 
-body.style.backgroundColor = "black";
+body.style.backgroundColor = "#93B20E";
+// body.style.backgroundImage = "url('../images/pngkey.com-boarders-png-790045.png')";
 main.style.backgroundColor = 'grey';
 // main.setAttribute('style', 'width: 900px; height: 900px;');
 // div.setAttribute('style', 'width: 900px; height: 900px;');
-main.setAttribute('style', 'width: auto; height: auto;');
-div.setAttribute('style', 'width: auto; height: auto;');
+// body.setAttribute('style', 'width: auto; height: auto;');
+main.setAttribute('style', 'width: 100%; height: 100%;');
+div.setAttribute('style', 'width: 100%; height: 100%; margin: auto; text-align: center; ');
 main.appendChild(div);
 div.setAttribute('id', 'sketchboard');
+sketchboard.setAttribute('style', 'height: 50%;')
 square.setAttribute('class', 'square');
 
 for (i = 0; i < 16; i++) {
     let row = document.createElement('div')
     row.setAttribute('class', 'row');
-    row.setAttribute('style', 'height: 22px; margin: 2px;')
+    row.setAttribute('style', 'height: 5%; margin: 2px; margin: auto;')
     for (j = 0; j < 16; j++) {
         let square = document.createElement('div');
         square.setAttribute('class', 'square');
-        square.setAttribute('style', 'width: 20px; height: 20px; background-color: grey; display: inline-block; margin: 1px;'); //square is a 50*50 grey blocks that line up
+        // square.setAttribute('style', 'width: 20px; height: 20px; background-color: #0F380F; text-align: center; display: inline-block; margin: 1px;'); //square is a 50*50 grey blocks that line up
         square.addEventListener('mouseover', function() {
-            square.style.backgroundColor = 'white';
+            square.style.backgroundColor = '#306230';
         });
         row.appendChild(square);
+        square.setAttribute('style', 'width: 5%; height: 95%; background-color: #0F380F; text-align: center; display: inline-block; margin: 1px;'); //square is a 50*50 grey blocks that line up
 
         let rowElement = document.getElementsByClassName('row');
         // let test = 'testing';
@@ -33,8 +37,22 @@ for (i = 0; i < 16; i++) {
     sketchboard.appendChild(row);
 };
 
+let button = document.getElementsByTagName('button');
+button[0].addEventListener('click', reset);
 
-div.style.border = '1px solid white';
+function reset() {
+    console.log('Resetting');
+    button[0].style.border = 'none';
+    let audio = new Audio('../images/gameboy_bootup.mp3');
+    audio.play();
+    let pixel = document.getElementsByClassName('square');
+    for (i = 0; i < pixel.length; i++) {
+        pixel[i].style.backgroundColor = "#0F380F";
+    };
+}
+
+
+// div.style.border = '1px solid white';
 
 
 /* getElementsByClassName() returns an HTML collection (array) */

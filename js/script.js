e47da1b0da72ea18c80d5ce2fc34704720c957e7
swifferwarrior <kdhailey21@gmail.com>
2020-06-07 16:30:13 -0500

    Add button to change grid
    Added grid button
    Added scanlines
    Gameboy overlay now scales more accurately on x/y axis

--- js/script.js
+++ js/script.js
@@ -16,39 +16,63 @@ div.setAttribute('id', 'sketchboard');
 sketchboard.setAttribute('style', 'height: 50%;')
 square.setAttribute('class', 'square');
 
-for (i = 0; i < 16; i++) {
-    let row = document.createElement('div')
-    row.setAttribute('class', 'row');
-    row.setAttribute('style', 'height: 5%; margin: 2px; margin: auto;')
-    for (j = 0; j < 16; j++) {
-        let square = document.createElement('div');
-        square.setAttribute('class', 'square');
-        // square.setAttribute('style', 'width: 20px; height: 20px; background-color: #0F380F; text-align: center; display: inline-block; margin: 1px;'); //square is a 50*50 grey blocks that line up
-        square.addEventListener('mouseover', function() {
-            square.style.backgroundColor = '#306230';
-        });
-        row.appendChild(square);
-        square.setAttribute('style', 'width: 5%; height: 95%; background-color: #0F380F; text-align: center; display: inline-block; margin: 1px;'); //square is a 50*50 grey blocks that line up
+// grid(16);
+// grid(5);
+grid(20);
 
-        let rowElement = document.getElementsByClassName('row');
-        // let test = 'testing';
-        // rowElement.innerHTML = test;
+function grid(n){
+    let frxHt = (1/n)*75;
+    let frxWdt = (1/n)*85;
+    console.log(frxHt);
+    for (i = 0; i < n; i++) { //height
+        let row = document.createElement('div')
+        row.setAttribute('class', 'row');
+        row.setAttribute('style', 'margin: 2px; margin: auto;')
+        row.style.height = frxHt + "%";
+        for (j = 0; j < n; j++) { //width
+            let square = document.createElement('div');
+            square.setAttribute('class', 'square');
+            // square.setAttribute('style', 'width: 20px; height: 20px; background-color: #0F380F; text-align: center; display: inline-block; margin: 1px;'); //square is a 50*50 grey blocks that line up
+            square.addEventListener('mouseover', function() {
+                square.style.backgroundColor = '#306230';
+            });
+            row.appendChild(square);
+            square.setAttribute('style', 'height: 95%; background-color: #0F380F; text-align: center; display: inline-block; margin: 1px;'); //square is a 50*50 grey blocks that line up
+            square.style.width = frxWdt + '%';
+    
+            let rowElement = document.getElementsByClassName('row');
+            // let test = 'testing';
+            // rowElement.innerHTML = test;
+        };
+        sketchboard.appendChild(row);
     };
-    sketchboard.appendChild(row);
+    
 };
 
 let button = document.getElementsByTagName('button');
-button[0].addEventListener('click', reset);
+button[0].addEventListener('click', newGrid);
+button[1].addEventListener('click', reset);
 
 function reset() {
     console.log('Resetting');
-    button[0].style.border = 'none';
+    button[1].style.border = 'none';
     let audio = new Audio('../images/gameboy_bootup.mp3');
     audio.play();
     let pixel = document.getElementsByClassName('square');
     for (i = 0; i < pixel.length; i++) {
         pixel[i].style.backgroundColor = "#0F380F";
     };
+    // grid();
+};
+
+function newGrid(){
+    sketchboard.innerHTML = "";
+    console.log('Sending popup');
+    console.log('How many squares?');
+    let number = prompt('How many? _');
+    console.log('Changing grid size to ' + number);
+    grid(number);
+
 }
 
 
